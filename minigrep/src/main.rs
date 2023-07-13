156dda3c71652d332e4640821a81da6c4ced7902
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-07-13 22:59:15 +0530

    struct Config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,12 +5,23 @@ fn main() {
     // read any command line arguments passed to it and then collect the values into vector
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1];
-    let file_path: &String =&args[2];
+    let config: Config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text: \n {contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query: &String = &args[1].clone();
+    let file_path: &String = &args[2].clone();
+
+    Config { query: query.to_string(), file_path: file_path.to_string() }
 }
