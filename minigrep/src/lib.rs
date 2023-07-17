5ac18190772f990f50fabcc4c4ff7928ba2ec28b
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-07-17 23:20:36 +0530

    tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -27,3 +27,32 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query: &str = "duct";
+        let contents: &str = "/
+Rust:
+safe, fast, productive
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+
+}
